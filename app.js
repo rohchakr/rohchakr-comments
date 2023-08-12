643066a0ed7dde563993b180f3cb68198007e281
rohchakr <rohchakr@gmail.com>
2023-08-12 12:09:17 +0530

    Custom attributes from html body

--- app.js
+++ app.js
@@ -1,1 +1,3 @@
 console.log("Initial code from comments app");
+var textFromPage = document.getElementById("rohchakr-comment-script").getAttribute("test-text");
+console.log("Text from test page " + textFromPage);
